c849e529ed703f74b796a5c64f761c5eb31ced7f
Adam Yost <swimmadude66@gmail.com>
2014-10-02 10:52:28 -0400

    assembler start

---
+++ Project 2/Assembler.py
@@ -0,0 +1,15 @@
+__author__ = 'ayost'
+
+
+# instruction dict
+    # key = Instruction word
+    # value = list
+
+# get .Name commands
+# get .orig
+
+
+
+fout = open('filename.mif', 'w')
+# wite out instructions
+fout.close()
